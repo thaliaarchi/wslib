051fc0711733183d0ad14ca1bc007161ffa64012
Andrew Archibald <andrew@aarchibald.com>
2021-05-25 23:19:23 -0700

    Add exp from Nebula and exp/modexp tests from Go

--- test.sh
+++ test.sh
@@ -5,6 +5,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+echo 'Testing math/bits.wsa'
 ./assemble.sh math/bits_test.wsa math/bits.wsa
 wspace prog.ws > prog.out
 diff math/bits_test.out prog.out
+
+echo 'Testing math/exp.wsa'
+./assemble.sh math/exp_test.wsa math/exp.wsa
+wspace prog.ws > prog.out
+diff math/exp_test.out prog.out
