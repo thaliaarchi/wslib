da90573cf06487b089b6db8ead4736df83cde02a
Andrew Archibald <andrew@aarchibald.com>
2021-05-27 19:31:43 -0700

    Assemble tests in build/; refactor test driver

--- assemble.sh
+++ assemble.sh
@@ -5,6 +5,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-gsed -Ef wsa.sed "$@" > prog.wsa
-wsc -f asm -t -o prog.ws prog.wsa
-nebula ir prog.ws > prog.ir 2>&1
+mkdir -p build
+prog="build/$(basename "${1%.wsa}")"
+gsed -Ef wsa.sed "$@" > "$prog.wsa"
+wsc -f asm -t -o "$prog.ws" "$prog.wsa"
+nebula ir "$prog.ws" > "$prog.ir" 2>&1
--- test.sh
+++ test.sh
@@ -1,26 +1,20 @@
-#!/bin/sh -e
+#!/bin/sh
 # Copyright (c) 2021 Andrew Archibald
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-echo 'Testing math/bits.wsa'
-./assemble.sh math/bits_test.wsa math/bits.wsa math/math.wsa
-wspace prog.ws > prog.out
-diff -q math/bits_test.out prog.out || :
+test() {
+  echo "Testing $1"
+  prog="build/$(basename "${1%.wsa}")"
+  want="${1%.wsa}.out"
+  ./assemble.sh "$@" &&
+    wspace "$prog.ws" > "$prog.out" &&
+    diff -q "$want" "$prog.out"
+}
 
-echo 'Testing math/collatz.wsa'
-./assemble.sh math/collatz_test.wsa math/collatz.wsa
-wspace prog.ws > prog.out
-diff -q math/collatz_test.out prog.out || :
-
-echo 'Testing math/exp.wsa'
-./assemble.sh math/exp_test.wsa math/exp.wsa math/math.wsa io/print.wsa
-wspace prog.ws > prog.out
-diff -q math/exp_test.out prog.out || :
-
-echo 'Testing io/print.wsa'
-./assemble.sh io/print_test.wsa io/print.wsa math/exp.wsa math/math.wsa
-wspace prog.ws > prog.out
-diff -q io/print_test.out prog.out || :
+test math/bits_test.wsa math/bits.wsa math/math.wsa
+test math/collatz_test.wsa math/collatz.wsa
+test math/exp_test.wsa math/exp.wsa math/math.wsa io/print.wsa
+test io/print_test.wsa io/print.wsa math/exp.wsa math/math.wsa
